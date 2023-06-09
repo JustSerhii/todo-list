6cfa14a7d0d9f6120ed61fa4ef9ef05025b2e77a
Serhii <scimitarnew@gmail.com>
2023-06-09 18:54:15 +0300

    change: addEventListener instead of Onclick

--- scripts/todo-list.js
+++ scripts/todo-list.js
@@ -16,18 +16,26 @@ function renderTodoList(){
     const html = `
       <div class="todo-name">${name}</div>
       <div>${dueDate}</div>
-      <button onclick="
-        todoList.splice(${i}, 1);
-        renderTodoList();
-        localSaveTodoList();
-      " class="delete-todo-button">Delete</button>
+      <button class="delete-todo-button js-delete-todo-button">Delete</button>
       `;
     todoListHTML += html;
   })
   
   document.querySelector('.js-todo-list')
     .innerHTML = todoListHTML;
+  
+  document.querySelectorAll('.js-delete-todo-button').forEach((deleteButton, index) => {
+    deleteButton.addEventListener('click', () => {
+      todoList.splice(index, 1);
+      renderTodoList();
+      localSaveTodoList();
+    })
+  });
 }
+
+document.querySelector('.js-add-todo-button').    addEventListener('click', () => {
+  addTodo();
+})
 
 function addTodo(){
   const inputElement = document.querySelector('.js-name-input');
